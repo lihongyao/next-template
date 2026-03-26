4947e6be8c756f6ba3a510a0e3477b5b08b04bca
Leo <lihy_online@163.com>
2026-03-27 00:21:26 +0400

    fix: 设置 zIndex

--- src/components/layouts/side-nav/Header.tsx
+++ src/components/layouts/side-nav/Header.tsx
@@ -1,5 +1,6 @@
 'use client';
 import Button from '@/components/ui/Button';
+import { ZIndex } from '@/constants';
 import { useModalRoutes } from '@/hooks/useModalRoutes';
 import { useBrandConfig } from '@/providers/brand.provider';
 import { useRouter } from '@/router';
@@ -10,7 +11,10 @@ export default function Header() {
   const { appName } = useBrandConfig();
   const { mergeRouteIntoCurrentPath } = useModalRoutes();
   return (
-    <header className="sticky top-0 left-0 flex h-[56px] w-full items-center justify-between bg-[#161616] px-3 text-white">
+    <header
+      className="sticky top-0 left-0 flex h-[56px] w-full items-center justify-between bg-[#161616] px-3 text-white"
+      style={{ zIndex: ZIndex.Header }}
+    >
       <h1 onClick={() => router.push(Routes.Home)}>{appName}</h1>
       <div className="flex items-center gap-3">
         <Button
