edd642bd150f43585ac0f4cdabba99d76713c555
Leo <leo@8u8.cc>
2026-04-08 18:36:03 +0400

    fix: LiquidGlass 参考资料

--- src/components/ui/LiquidGlass/index.tsx
+++ src/components/ui/LiquidGlass/index.tsx
@@ -1,6 +1,10 @@
 /**
  * https://juejin.cn/post/7514618352829448244
  * https://www.npmjs.com/package/liquid-glass-react
+ * https://kube.io/blog/liquid-glass-css-svg/
+ * https://juejin.cn/post/7514618352829448244
+ * https://www.liquid-glass.pro/generator.html
+ * https://juejin.cn/post/7515390154299818021?searchId=20260317182921FF9FB8D661A6C66D26C1
  */
 import { cn } from '@/libs/class-helpers';
 
