5723c6d7d224ef4a7e4c13e98b70547be3d87880
Leo <lihy_online@163.com>
2026-05-07 20:45:34 +0400

    fix: update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -55,13 +55,6 @@ export default withSentryConfig(withNextIntl(nextConfig), {
 
   // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: true,
-
-  // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
-  // tunnelRoute: "/monitoring",
-
   webpack: {
     // Tree-shaking options for reducing bundle size
     treeshake: {
