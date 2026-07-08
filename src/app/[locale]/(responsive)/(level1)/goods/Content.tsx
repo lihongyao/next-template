1798b5449cbb3683ab3d3c203bbffbc50d37b372
Leo <lihy_online@163.com>
2026-07-08 17:28:12 +0400

    fix: 样式调整

--- src/app/[locale]/(responsive)/(level1)/goods/Content.tsx
+++ src/app/[locale]/(responsive)/(level1)/goods/Content.tsx
@@ -4,16 +4,21 @@ import { Link } from '@/router';
 
 export default function Content({ json }: { json: any }) {
   const data = use(json);
+  console.log(data);
   return (
-    <main className="grid grid-cols-2 gap-3 p-3 sm:grid-cols-5 md:grid-cols-7">
+    <main className="grid grid-cols-2 gap-3 p-3 sm:grid-cols-3 md:grid-cols-5">
       {data.products.map((item: any) => (
         <Link
           key={item.id}
           href={`/news/${item.id}?ch=123`}
-          className="block bg-amber-50 text-black"
+          className="block rounded-lg bg-[#212121] px-4 pb-4 text-black"
         >
-          <img src={item.thumbnail} />
-          {item.brand}
+          <img className="h-auto w-full" src={item.thumbnail} />
+          <div className="line-clamp-1 font-bold text-white">{item.title}</div>
+          <div className="flex items-center justify-between">
+            <div className="text-lg font-bold text-red-500">&yen;{item.price}</div>
+            <div className="text-[#B3B8C1]">折扣:{item.discountPercentage}%</div>
+          </div>
         </Link>
       ))}
     </main>
